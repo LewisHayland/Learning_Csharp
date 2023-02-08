c940fb16d702133f5de6314b7e7c19427c6029c6
LewisHayland <118905529+LewisHayland@users.noreply.github.com>
2023-02-08 11:45:04 +0000

    Add files via upload

---
+++ testing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect(4, 5);
+            Circle(4);
+            Tri(10, 9);
+        }
+        static void Rect(double length, double width)
+        {
+            Console.WriteLine($"The Area of the rectabgle is {length * width}.");
+        }
+        static void Circle(double radius)
+        {
+            Console.WriteLine($"The Area of the circle is {(Math.PI * Math.Pow(radius, 2))}.");
+        }
+        static void Tri(double bottom, double height)
+        {
+            Console.WriteLine($"The Area of the Triangle is {0.5 * bottom * height}");
+        }
+        static void Calculate_Cost()
+        {
+
+        }
+
+
+    }
+}
